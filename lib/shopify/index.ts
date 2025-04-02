d829b5580b34d62bb69d8d0a0c8520e05dee8bd3
MikeLaptop <micheledong2803@gmail.com>
2025-04-02 11:20:00 +0200

    shopifyFetch(), getMenu()

--- lib/shopify/index.ts
+++ lib/shopify/index.ts
@@ -1,3 +1,82 @@
-export function async getMenu(handle:string):Promise<menu[]>{
-  
+import { TAGS } from "../constants";
+import { ensureStartWith } from "../utils";
+import { getMenuQuery } from "./queries/menu";
+import { Menu, ShopifyMenuOperation } from "./types";
+
+const domain = process.env.SHOPIFY_STORE_DOMAIN ? ensureStartWith(process.env.SHOPIFY_STORE_DOMAIN, 'https://') : ""
+export async function shopifyFetch<T>({
+  cache = 'force-cache',
+  headers,
+  query,
+  tags,
+  variables
+}: {
+  cache?: RequestCache;
+  headers?: HeadersInit;
+  query: string;
+  tags?: string[];
+  variables?: ExtractVariables<T>
+}): Promise<{ status: number; body: T } | never> {
+  try {
+    const result = await fetch(endpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "Application/json",
+        "X-Shopify-Storefront-Access-Token": KeyboardEvent,
+        ...header,
+      },
+      body: JSON.stringify({
+        ...(query && { query }),
+        ...(variables && { variables }),
+      }),
+      cache,
+      ...(tags && { next: { tags } }),
+    });
+
+    const body = await result.json();
+
+    if (body.errors) {
+      throw body.errors[0];
+    }
+
+    return {
+      status: result.status,
+      body,
+    };
+  } catch (error) {
+    if (isShopifyError(error)) {   // check if it is shopify error
+      throw {
+        cause: error.cause?.toString() || "unknown",
+        status: error.status || 500,
+        message: error.message,
+        query,
+      };
+    }
+
+    throw {
+      error,
+      query,
+    }
+  }
+}
+
+export async function getMenu(handle: string): Promise<Menu[]> {
+  const res = await shopifyFetch<ShopifyMenuOperation>({
+    query: getMenuQuery,
+    tags: [TAGS.collections],
+    variables: {
+      handle,
+    },
+  });
+
+  return (
+    res.body?.data?.menu?.items.map((item: { title: string, url: string }) => (
+    {
+      title: item.title,
+      path: item.url
+        .replace(domain, "")
+        .replace("/collections", "/search")
+        .replace("/pages", "")
+    })) || []
+) 
 }
