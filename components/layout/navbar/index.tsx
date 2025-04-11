c97230c6d5a0810d7f2a10fa2f4fdead5adc545a
MikeLaptop <micheledong2803@gmail.com>
2025-04-12 01:06:09 +0200

    navbar broke

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -3,15 +3,15 @@ import { Menu } from "@/lib/shopify/types";
 import Link from "next/link";
 import MobileMenu from "./mobile-menu";
 import Search from "./search";
+import Logo from "@/components/icons/logo";
 
 export async function Navbar() {
-  const SITE_NAME = process.env.SITE_NAME;
   const menu = await getMenu("next-js-frontend-menu");
   console.log(menu);
   return (
     <nav className="flex items-center justify-between p-4 lg:px-6">
       <div className="block flex-none md:hidden">
-        <MobileMenu menu={menu}/>
+        <MobileMenu menu={menu} />
       </div>
       <div className="flex w-full items-center">
         <div className="flex w-full md:w-1/3">
@@ -20,36 +20,32 @@ export async function Navbar() {
             prefetch={true}
             className="mr-2 flex w-full items-center justify-center md:w-auto lg:mr-6"
           >
-            Home
+            <Logo />
             <div className="ml-2 flex-none text-sm font-medium uppercase md:hidden lg:block">
-              {SITE_NAME}
+              {process.env.SITE_NAME}
             </div>
           </Link>
 
           {menu.length > 0 ? (
             <ul className="hidden gap-6 text-sm md:flex md:items-center">
-              {menu.map((item: Menu) => {
-                return (
-                  <li key={item.title}>
-                    <Link
-                      href={item.path}
-                      prefetch={true}
-                      className="text-gray-700 underline-offset-4 hover:black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300"
-                    >
-                      {item.title}
-                    </Link>
-                  </li>
-                );
-              })}
+              {menu.map((item: Menu) => (
+                <li key={item.title}>
+                  <Link
+                    href={item.path}
+                    prefetch={true}
+                    className="text-gray-700 underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300"
+                  >
+                    {item.title}
+                  </Link>
+                </li>
+              ))}
             </ul>
           ) : null}
         </div>
         <div className="hidden justify-center md:flex md:w-1/3">
           <Search />
         </div>
-        <div className="flex justify-end md:w-1/3">
-          <CartModal />
-        </div>
+        <div className="flex justify-end md:w-1/3">{/* <CartModal /> */}</div>
       </div>
     </nav>
   );
