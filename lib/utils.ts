cf316179465dd47186fff57cad0d6e08999865dc
MikeLaptop <micheledong2803@gmail.com>
2025-04-12 00:21:00 +0200

    search component

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,15 @@
+import { ReadonlyURLSearchParams } from "next/navigation";
+
 export function ensureStartWith(check: string, start: string) {
   return check.startsWith(start) ? check : `${start}${check}`;
+}
+
+export function createUrl(
+  pathname: string,
+  params: URLSearchParams | ReadonlyURLSearchParams
+) {
+  const paramsString = params.toString();
+  const queryString = `${paramsString.length ? "?" : ""}${paramsString}`;
+
+  return `${pathname}${queryString}`;
 }
